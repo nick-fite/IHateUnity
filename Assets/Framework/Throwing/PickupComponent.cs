1d5230e144d8a89f576b8b647a45b7a6242b5a1a
nick-fite <70393136+nick-fite@users.noreply.github.com>
2024-12-11 07:04:30 -0600

    dumb

--- Assets/Framework/Throwing/PickupComponent.cs
+++ Assets/Framework/Throwing/PickupComponent.cs
@@ -64,11 +64,7 @@ public class PickupComponent : NetworkBehaviour, IinteractionInterface
 
                 Vector3 throwDir = player.GetPlayerForward() + player.GetPlayerUp();
                 Debug.Log("launch in pickup");
-<<<<<<< HEAD
-                _launchComponent.CheckLaunch(throwDir, 4f, throwComp);
-=======
                 _launchComponent.CheckLaunch(throwDir, throwComp.GetThrowVelocity(), throwComp);
->>>>>>> f3d7c7153387e1222a46b5882708ecfc72d9782d
             }
         }
     }
